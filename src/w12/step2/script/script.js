71093a535a983fd2495536300450a5ec74d5d683
Jaehun-Kang <74220533+Jaehun-Kang@users.noreply.github.com>
2025-08-21 13:16:57 +0900

    최신화

---
+++ src/w12/step2/script/script.js
@@ -0,0 +1,17 @@
+console.log('안녕하세요?');
+let elemBody = document.body;
+console.log(elemBody);
+elemBody.style.backgroundColor = 'oklch(0.8, 0.05, 0)';
+// oklch javascript doesn't work(chrome://flags)
+
+let aCard = document.querySelector('.card');
+console.log(aCard);
+// html엘레멘트.addEventListener('이벤트 종류', function() {할일});
+aCard.addEventListener('click', function () {
+  console.log('클릭!');
+  // aCard.style.backgroundColor = '#ff0000';
+  aCard.classList.toggle('card--state-clicked');
+  console.log(aCard.classList);
+});
+
+// document.querySelectorAll('.card');
